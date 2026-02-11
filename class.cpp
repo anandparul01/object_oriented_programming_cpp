2fb2fc4a92396319ff73d603d18b5e760b67aa0d
anandparul01 <londonparul@gmail.com>
2026-02-11 09:13:41 +0000

    First commit

---
+++ class.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+// class - describes the structure
+class Person{
+    public:
+    string name; //data member
+    string surname; // data member
+
+    //methods - functions defined in the class
+    void printFullName(){ 
+        cout << name << " " << surname << endl;
+    }
+};
+
+int main() {
+    // object - specific example of that structure.. also called instance. eg p and p2 in this case
+    Person p; // instantiating an object of the class Person
+    p.name = "Parul";
+    p.surname = "Anand";
+
+    p.printFullName();
+
+    Person p2;
+    p2.name = "John";
+    p2.surname = "Doe";
+
+    p2.printFullName();
+}
